b82fbcb9c8144ad56cb460e9e046933cd802ebe4
HyunsukLee <hyunsuk76@gmail.com>
2021-09-16 15:38:05 +0900

    Add files via upload

---
+++ src/sampleCode/SampleCode.java
@@ -0,0 +1,31 @@
+package sampleCode;
+
+import java.util.Scanner;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+
+		int A, B, addresult; 
+		
+		Scanner scn = new Scanner(System.in); 
+		A = scn.nextInt(); 
+		B = scn.nextInt(); 
+		addresult = A + B; 
+		System.out.println(addresult);
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		while(sc.hasNext()) { 
+			System.out.println(sc.nextLine()); 
+		}
+		
+		System.out.println("TEST");
+		
+		// 2021.09.16 modify v.0.1
+		// 2021.09.16 modify v.0.2	
+
+	
+	}
+
+}
